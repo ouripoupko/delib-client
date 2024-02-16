1617c28e39a7bad9ea30fb74df09269a7769cc8a
ouripoupko <ouri.poupko@gmail.com>
2024-02-16 20:40:32 +0200

    fix base url

--- src/app/agent.service.ts
+++ src/app/agent.service.ts
@@ -20,11 +20,11 @@ export class AgentService {
     private http: HttpClient) { }
 
   listen(server: string, identity: string, contract: string): EventSource {
-    return new EventSource(`${server}stream?agent=${identity}&contract=${contract}`);
+    return new EventSource(`${server}/stream?agent=${identity}&contract=${contract}`);
   }
 
   write(server: string, identity: string, contract: string, method: Method): Observable<any> {
-    const url = `${server}ibc/app/${identity}/${contract}/${method.name}`;
+    const url = `${server}/ibc/app/${identity}/${contract}/${method.name}`;
     let params = new HttpParams().set('action', 'contract_write');
     console.log(url);
     return this.http.post<any>(url, method, {...this.httpOptions, params: params}).pipe(
@@ -35,7 +35,7 @@ export class AgentService {
   }
 
   read(server: string, identity: string, contract: string, method: Method): Observable<any> {
-    const url = `${server}ibc/app/${identity}/${contract}/${method.name}`;
+    const url = `${server}/ibc/app/${identity}/${contract}/${method.name}`;
     let params = new HttpParams().set('action', 'contract_read');
     return this.http.post<any>(url, method, {...this.httpOptions, params: params}).pipe(
       tap(_ => console.log('read something')),
